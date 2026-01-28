e295c9c3af80ba76e53b7091589039f3bbd4a964
Pranjeet <prgoswami2020@gmail.com>
2026-01-28 18:25:22 +0530

    added

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,8 +1,5 @@
-"use client"
-import { menuOptions } from '@/src/lib/constant'
-import clsx from 'clsx'
+'use client'
 import Link from 'next/link'
-import Image from 'next/image'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import {
@@ -10,53 +7,95 @@ import {
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
-} from "@/components/ui/tooltip"
+} from '@/components/ui/tooltip'
+import { menuOptions } from '@/src/lib/constant'
+import clsx from 'clsx'
 import { Separator } from '@/components/ui/separator'
+import { Database, GitBranch, LucideMousePointerClick } from 'lucide-react'
+import { ModeToggle } from '../global/mode-toggle'
 
 type Props = {}
 
 const MenuOptions = (props: Props) => {
-    const pathName = usePathname();
+    const pathName = usePathname()
 
     return (
-        <nav className="dark:bg-black h-screen overflow-scroll justify-between flex items-center flex-col gap-10 py-6 px-2">
-            <div className='flex items-center justify-center flex-col gap-8'>
-                <Link className="flex font-bold flex-row" href="/">
-                    <Image
-                        src="/Images/logo.png"
-                        alt="Logo"
-                        width={24}
-                        height={24}
-                    />
+        <nav className=" dark:bg-black h-screen overflow-scroll  justify-between flex items-center flex-col  gap-10 py-6 px-2">
+            <div className="flex items-center justify-center flex-col gap-8">
+                <Link
+                    className="flex font-bold flex-row "
+                    href="/"
+                >
+                    fuzzie.
                 </Link>
                 <TooltipProvider>
-                    <ul className="flex flex-col gap-8">
-                        {menuOptions.map((menuItem) => (
-                            <li key={menuItem.name}>
-                                <Tooltip delayDuration={0}>
-                                    <TooltipTrigger asChild>
-                                        <Link href={menuItem.href}
+                    {menuOptions.map((menuItem) => (
+                        <ul key={menuItem.name}>
+                            <Tooltip delayDuration={0}>
+                                <TooltipTrigger>
+                                    <li>
+                                        <Link
+                                            href={menuItem.href}
                                             className={clsx(
-                                                'group h-8 w-8 flex items-center justify-center scale-[1.5] rounded-lg p-[3px] cursor-pointer', { 'dark:bg-[#2F006B] bg-[#EE0FF]': pathName === menuItem.href, }
+                                                'group h-8 w-8 flex items-center justify-center  scale-[1.5] rounded-lg p-[3px]  cursor-pointer',
+                                                {
+                                                    'dark:bg-[#2F006B] bg-[#EEE0FF] ':
+                                                        pathName === menuItem.href,
+                                                }
                                             )}
                                         >
                                             <menuItem.Component
                                                 selected={pathName === menuItem.href}
                                             />
                                         </Link>
-                                    </TooltipTrigger>
-                                    <TooltipContent side="right">
-                                        <p>{menuItem.name}</p>
-                                    </TooltipContent>
-                                </Tooltip>
-                            </li>
-                        ))}
-                    </ul>
+                                    </li>
+                                </TooltipTrigger>
+                                <TooltipContent
+                                    side="right"
+                                    className="bg-black/10 backdrop-blur-xl"
+                                >
+                                    <p>{menuItem.name}</p>
+                                </TooltipContent>
+                            </Tooltip>
+                        </ul>
+                    ))}
                 </TooltipProvider>
                 <Separator />
+                <div className="flex items-center flex-col gap-9 dark:bg-[#353346]/30 py-4 px-2 rounded-full h-56 overflow-scroll border-[1px]">
+                    <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+                        <LucideMousePointerClick
+                            className="dark:text-white"
+                            size={18}
+                        />
+                        <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]" />
+                    </div>
+                    <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+                        <GitBranch
+                            className="text-muted-foreground"
+                            size={18}
+                        />
+                        <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+                    </div>
+                    <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+                        <Database
+                            className="text-muted-foreground"
+                            size={18}
+                        />
+                        <div className="border-l-2 border-muted-foreground/50 h-6 absolute left-1/2 transform translate-x-[-50%] -bottom-[30px]"></div>
+                    </div>
+                    <div className="relative dark:bg-[#353346]/70 p-2 rounded-full dark:border-t-[2px] border-[1px] dark:border-t-[#353346]">
+                        <GitBranch
+                            className="text-muted-foreground"
+                            size={18}
+                        />
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-center justify-center flex-col gap-8">
+                <ModeToggle />
             </div>
         </nav>
     )
 }
 
-export default MenuOptions;
+export default MenuOptions
